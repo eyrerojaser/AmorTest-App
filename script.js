2dddb7de46063d5c3e08ef432b0ba56437cf74ad
eyrerojaser <eyre.rojas.er@gmail.com>
2025-11-02 10:18:50 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const quiz = [
+  { question: "¿Te sientes escuchada/o por tu pareja?", scoreIfYes: 1 },
+  { question: "¿Te apoya en tus metas y sueños?", scoreIfYes: 1 },
+  { question: "¿Te hace sentir culpable o inferior?", scoreIfYes: -1 },
+  { question: "¿Sientes libertad para expresarte con sinceridad?", score
